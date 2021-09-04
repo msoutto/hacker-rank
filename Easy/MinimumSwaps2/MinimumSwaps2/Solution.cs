ee5c159589722b07c322fe5085040745aef65110
Matheus Soutto <matheussoutto27@gmail.com>
2021-09-03 23:25:13 -0300

    MinimumSwaps2

---
+++ Easy/MinimumSwaps2/MinimumSwaps2/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MinimumSwaps2
+{
+    class Solution
+    {
+        // Complete the minimumSwaps function below.
+        static int minimumSwaps(int[] arr)
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // Array should always be {1,2,3,...,n} (challenge description)
+                while (arr[i] > i + 1)
+                {
+                    // If it is greater, swap with its correct position
+                    Console.WriteLine("Swap({0},{1})", i, arr[i] - 1);
+                    swap(ref arr, i, arr[i] - 1);
+                    Console.WriteLine(String.Concat(Array.ConvertAll(arr, item => $"{item} ")));
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void swap(ref int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int res = minimumSwaps(arr);
+
+            Console.WriteLine(res);
+        }
+    }
+}
