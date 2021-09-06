8c497097431a38cc8a9dedf8ef01e5efb3d027b1
Matheus Soutto <matheussoutto27@gmail.com>
2021-09-06 01:28:36 -0300

    Sorting algorithm: Insertion Sort

---
+++ Sorting/InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertionSort
+{
+    public static class InsertionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int unsortedItem;
+
+            // Starts at the unsorted part of the array
+            for (int i = 1; i < arr.Length; i++)
+            {
+                // j is the index of the sorted part of the array
+                // initially we consider the first element to be the sorted part
+                int j = i - 1;
+                unsortedItem = arr[i];
+
+                while (j >= 0 && arr[j] > unsortedItem)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                // Insert the unsortedItem in its correct position,
+                // being now in the sorted part of the array (also being sorted)
+                arr[j + 1] = unsortedItem;
+            }
+        }
+    }
+}
