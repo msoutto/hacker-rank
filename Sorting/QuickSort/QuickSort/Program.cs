10b872a2af620fca1d421f5c2568a62647a8b12d
Matheus Soutto <matheussoutto27@gmail.com>
2021-09-05 23:32:14 -0300

    Sorting algorithm: Quick Sort

---
+++ Sorting/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1000, 2000, 1500, 500, 100, 300 };
+            int[] arr2 = new int[] { 20, 38, 10, 29, 33, 50, 15 };
+
+            Console.Write("arr1: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr1, item => $"{item} ")));
+            QuickSort.Sort(arr1);
+            Console.Write("sorted arr1: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr1, item => $"{item} ")));
+
+            Console.Write("arr2: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr2, item => $"{item} ")));
+            QuickSort.Sort(arr2);
+            Console.Write("sorted arr2: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr2, item => $"{item} ")));
+
+        }
+    }
+}
---
+++ Sorting/QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    public static class QuickSort
+    {
+        public static void Sort(int[] arr, int low = 0, int high = Int32.MaxValue)
+        {
+            if (high > arr.Length - 1)
+                high = arr.Length - 1;
+
+
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+
+                Sort(arr, low, pi - 1);
+                Sort(arr, pi + 1, high);
+            }
+        }
+
+        static int Partition(int[] arr, int low, int high)
+        {
+            // My little improvement... :)
+            if (arr[low] > arr[high])
+                Swap(arr, low, high);
+
+            int j = low - 1;
+
+            for (int i = low; i < high; i++)
+            {// Is the current item greater than pivot?
+                if (arr[i] <= arr[high])
+                {// Swap current with the position in front of the last swapped
+                 // (last item less than pivot)
+                    j++;
+                    Swap(arr, i, j);
+                }
+            }
+            // Swap the pivot with the position in front of the last swapped
+            // (last item less than pivot)
+            j++;
+            Swap(arr, j, high);
+
+            return j;
+        }
+
+        static void Swap(int[] arr, int index1, int index2)
+        {
+            if (index1 != index2)
+            {
+                int temp = arr[index1];
+                arr[index1] = arr[index2];
+                arr[index2] = temp;
+            }
+        }
+    }
+}
