1c49bc37a62fcaa8e861ebc1528cf3d8a5559a8d
Matheus Soutto <matheussoutto27@gmail.com>
2021-09-07 01:37:51 -0300

    Sorting Algorithm: Selection Sort

---
+++ SortingAlgorithms/SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectionSort
+{
+    public static class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int sorted = -1, minimum;
+
+            for (int unsorted = 0; unsorted < arr.Length; unsorted++)
+            {
+                minimum = unsorted;
+                for (int i = unsorted + 1; i < arr.Length; i++)
+                {
+                    if (arr[i] < arr[minimum])
+                        minimum = i;
+                }
+
+                sorted++;
+                Swap(arr, sorted, minimum);
+            }
+        }
+
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
---
+++ SortingAlgorithms/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Selection;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[10];
+            int[] arr2 = new int[15];
+
+            Random random = new Random();
+            // Fill arrays
+            for (int i = 0; i < arr1.Length; i++) arr1[i] = random.Next(arr1.Length);
+            for (int i = 0; i < arr2.Length; i++) arr2[i] = random.Next(arr2.Length);
+
+            Console.Write("arr1: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr1, item => $"{item} ")));
+            SelectionSort.Sort(arr1);
+            Console.Write("sorted arr1: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr1, item => $"{item} ")));
+
+            Console.Write("arr2: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr2, item => $"{item} ")));
+            SelectionSort.Sort(arr2);
+            Console.Write("sorted arr1: ");
+            Console.WriteLine(String.Concat(Array.ConvertAll(arr2, item => $"{item} ")));
+        }
+    }
+}
