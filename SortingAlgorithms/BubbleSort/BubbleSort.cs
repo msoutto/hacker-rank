16a2ac335440da5cbb87a94ba5878293f8ad44ed
Matheus Soutto <matheussoutto27@gmail.com>
2021-09-07 12:24:28 -0300

    Sorting Algorithms: Bubble Sort

---
+++ SortingAlgorithms/BubbleSort/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bubble
+{
+    public static class BubbleSort
+    {
+        public static void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                // arr.Length - i - 1 is the sorted part, as the iterations happen
+                // the greatest element will be pushed to the last index
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                        Swap(arr, j, j + 1);
+                }
+            }
+        }
+
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
